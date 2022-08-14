f1e3ee1eef6af2137b3665aa3f218796b64964a1
emilyaikens <emily.c.aikens@gmail.com>
2022-08-13 21:10:57 -0700

    add dom object with dom select elements

---
+++ main.js
@@ -0,0 +1,11 @@
+let dom = {
+    html: document.getElementById("html"),
+    css: document.getElementById("css"),
+    js: document.getElementById("js"),
+    card: document.querySelector(".card"),
+    question: document.querySelector(".question"),
+    answer: document.querySelector(".answer"),
+    back: document.getElementById("back"),
+    answer: document.getElementById("answer"),
+    next: document.getElementById("next")
+};
