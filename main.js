d3941eb3e84724df07bb6f0b9542714eb550d669
emilyaikens <emily.c.aikens@gmail.com>
2022-08-14 18:36:55 -0700

    put next, answer, back button functions within html, css, js button functions. Not dry.

--- main.js
+++ main.js
@@ -43,29 +43,18 @@ card1.addCard();
 let card2 = new flashCard("second question", "second answer", "html");
 card2.addCard();
 
+//HTML BUTTON - loads html card array
 dom.html.addEventListener("click", function() {
     vars.htmlCalls = true;
     vars.cssCalls = false;
     vars.jsCalls = false;
-});
+    vars.scrollClicks = 0;
+    console.log("if thing is working html");
+    dom.question.innerHTML = vars.htmlCards[0].question;
+    dom.answer.innerHTML = vars.htmlCards[0].answer;
 
-dom.css.addEventListener("click", function() {
-    vars.htmlCalls = false;
-    vars.cssCalls = true;
-    vars.jsCalls = false;
-});
-
-dom.js.addEventListener("click", function() {
-    vars.htmlCalls = false;
-    vars.cssCalls = false;
-    vars.jsCalls = true;
-});
-
-dom.question.innerHTML = vars.htmlCards[0].question;
-dom.answer.innerHTML = vars.htmlCards[0].answer;
-
-//ANSWER BUTTON
-dom.answerButton.addEventListener("click", function() {
+    //answer button
+    dom.answerButton.addEventListener("click", function() {
     dom.card.classList.toggle("flipped");
     vars.answerClicks++;
         if (dom.answerButton.innerHTML === "Answer") {
@@ -73,9 +62,9 @@ dom.answerButton.addEventListener("click", function() {
         } else if (dom.answerButton.innerHTML === "Question") {
             dom.answerButton.innerHTML = "Answer";
         };
-});
-//NEXT BUTTON
-dom.next.addEventListener("click", function() {
+    });
+    //next button
+    dom.next.addEventListener("click", function() {
         dom.answerButton.innerHTML = "Answer";
         vars.scrollClicks++;
     if (vars.answerClicks %2 === 0) {
@@ -90,9 +79,9 @@ dom.next.addEventListener("click", function() {
             vars.answerClicks = 0;
             },0500);
     }
-});
-//BACK BUTTON
-dom.back.addEventListener("click", function() {
+    });
+    //back button
+    dom.back.addEventListener("click", function() {
     if (dom.answerButton.innerHTML === "Question") {
         dom.answerButton.innerHTML = "Answer";
     };
@@ -108,4 +97,72 @@ dom.back.addEventListener("click", function() {
             dom.answer.innerHTML = vars.htmlCards[vars.scrollClicks].answer;
             },0500);
     }
+    });
 });
+//CSS BUTTON - loads css card array
+dom.css.addEventListener("click", function() {
+    console.log("click css");
+    vars.htmlCalls = false;
+    vars.cssCalls = true;
+    vars.jsCalls = false;
+    console.log("if thing is working css");
+    vars.scrollClicks = 0;
+    dom.question.innerHTML = vars.cssCards[0].question;
+    dom.answer.innerHTML = vars.cssCards[0].answer;
+
+    //answer button
+    dom.answerButton.addEventListener("click", function() {
+    dom.card.classList.toggle("flipped");
+    vars.answerClicks++;
+        if (dom.answerButton.innerHTML === "Answer") {
+            dom.answerButton.innerHTML = "Question";
+        } else if (dom.answerButton.innerHTML === "Question") {
+            dom.answerButton.innerHTML = "Answer";
+        };
+    });
+    //next button
+    dom.next.addEventListener("click", function() {
+        dom.answerButton.innerHTML = "Answer";
+        vars.scrollClicks++;
+    if (vars.answerClicks %2 === 0) {
+        dom.question.innerHTML = vars.cssCards[vars.scrollClicks].question;
+        dom.answer.innerHTML = vars.cssCards[vars.scrollClicks].answer;
+        vars.answerClicks = 0;
+    } else {
+        dom.card.classList.toggle("flipped");
+        setTimeout(function (){
+            dom.question.innerHTML = vars.cssCards[vars.scrollClicks].question;
+            dom.answer.innerHTML = vars.cssCards[vars.scrollClicks].answer;
+            vars.answerClicks = 0;
+            },0500);
+    }
+    });
+    //back button
+    dom.back.addEventListener("click", function() {
+    if (dom.answerButton.innerHTML === "Question") {
+        dom.answerButton.innerHTML = "Answer";
+    };
+    if (vars.answerClicks %2 === 0) {
+        vars.scrollClicks--;
+        dom.question.innerHTML = vars.cssCards[vars.scrollClicks].question;
+        dom.answer.innerHTML = vars.cssCards[vars.scrollClicks].answer;
+    } else {
+        dom.card.classList.toggle("flipped");
+        setTimeout(function (){
+            vars.scrollClicks--;
+            dom.question.innerHTML = vars.cssCards[vars.scrollClicks].question;
+            dom.answer.innerHTML = vars.cssCards[vars.scrollClicks].answer;
+            },0500);
+    }
+    });
+});
+
+dom.js.addEventListener("click", function() {
+    vars.htmlCalls = false;
+    vars.cssCalls = false;
+    vars.jsCalls = true;
+});
+
+// if (vars.htmlCalls === true) {
+    
+// };
